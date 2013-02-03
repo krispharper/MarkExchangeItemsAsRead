33ac61a2ec4efffedd1d8c705057a8acc4ccf7b4
Kris harper <kris.p.kharper@gmail.com>
2013-02-03 13:20:30 -0500

    Made the bulk of the program, copying it over from a PowerShell script.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.DirectoryServices.AccountManagement;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Exchange.WebServices.Data;
 
 namespace MarkExchangeItemsAsRead
 {
@@ -10,6 +9,39 @@ namespace MarkExchangeItemsAsRead
     {
         static void Main(string[] args)
         {
+            ExchangeService service = new ExchangeService(ExchangeVersion.Exchange2007_SP1);
+            service.AutodiscoverUrl(UserPrincipal.Current.EmailAddress);
+            Folder inbox = Folder.Bind(service, WellKnownFolderName.Inbox);
+
+            FolderView folderView = new FolderView(inbox.ChildFolderCount);
+            folderView.PropertySet = new PropertySet(BasePropertySet.IdOnly, FolderSchema.DisplayName, FolderSchema.ChildFolderCount);
+            SearchFilter filter = new SearchFilter.IsEqualTo(FolderSchema.DisplayName, "Notifications");
+
+            Folder folder = inbox.FindFolders(filter, folderView).First();
+            MarkAllAsRead(folder);
+        }
+
+        static void MarkAllAsRead(Folder folder)
+        {
+            if (folder.ChildFolderCount == 0)
+            {
+                ItemView itemView = new ItemView(100);
+                itemView.PropertySet = new PropertySet(BasePropertySet.IdOnly, EmailMessageSchema.IsRead);
+                SearchFilter filter = new SearchFilter.IsEqualTo(EmailMessageSchema.IsRead, false);
+                folder.FindItems(filter, itemView)
+                      .Cast<EmailMessage>()
+                      .ToList()
+                      .ForEach(item =>
+                {
+                    item.IsRead = true;
+                    item.Update(ConflictResolutionMode.AutoResolve);
+                });
+            }
+            else
+            {
+                FolderView folderView = new FolderView(folder.ChildFolderCount);
+                folder.FindFolders(folderView).ToList().ForEach(child => MarkAllAsRead(child));
+            }
         }
     }
 }
